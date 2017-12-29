4a8de0dc4edac0ff009bef9b1cdc63a57a845803
Mateus Rodrigues <devrdgs@gmail.com>
2017-12-29 11:59:09 -0200

    Rep started

---
+++ src/image_viewer.js
@@ -0,0 +1,5 @@
+import '../styles/image_viewer.css';
+
+const image = document.createElement('img');
+image.src = 'http://via.placeholder.com/400x400';
+document.body.appendChild(image);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
+
+module.exports = config;
